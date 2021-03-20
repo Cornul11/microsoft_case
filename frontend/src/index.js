b37cb6cb3835cabab30fcde1eefcd9a81c1736ff
lesslyrics <alina.boshchenko@jetbrains.com>
2021-03-20 12:31:56 +0300

    design

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,30 @@ import 'antd/dist/antd.css';
 import App from "./App"
 import DragAndDrop from "./DragAndDrop";
 import DBPedia from "./bot/DBPedia";
+import {ThemeProvider} from "styled-components";
 
 ReactDOM.render(<DragAndDrop />, document.getElementById('root'));
+// all available props
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#91bbb3',
+  headerFontColor: '#fff',
+  headerFontSize: '17px',
+  botBubbleColor: '#91bbb3',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
 ReactDOM.render(<React.StrictMode>
         <App/>
     </React.StrictMode>,
     document.getElementById('root'));
 
-ReactDOM.render(<DBPedia/>, document.getElementById('bot'));
+ReactDOM.render(<ThemeProvider theme={theme}>
+    <DBPedia />;
+  </ThemeProvider>, document.getElementById('bot'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
