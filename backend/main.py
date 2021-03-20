b06a23c1be0aecc19e8997b8065ce8f2b36e7abf
Ion Babalau <ionbabalau2020@gmail.com>
2021-03-20 19:02:43 +0200

    Add Immersive Reader js implementation

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+from dotenv import load_dotenv
+from os import environ
+from requests import post
+import json
+
+load_dotenv()
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/GetTokenAndSubdomain', methods=['GET'])
+def getTokenAndSubdomain():
+    'Get the access token'
+    if request.method == 'GET':
+        try:
+            headers = { 'content-type': 'application/x-www-form-urlencoded' }
+            data = {
+                'client_id': str(os.environ.get('CLIENT_ID')),
+                'client_secret': str(os.environ.get('CLIENT_SECRET')),
+                'resource': 'https://cognitiveservices.azure.com/',
+                'grant_type': 'client_credentials'
+            }
+
+            resp = requests.post('https://login.windows.net/' + str(os.environ.get('TENANT_ID')) + '/oauth2/token', data=data, headers=headers)
+            jsonResp = resp.json()
+
+            if ('access_token' not in jsonResp):
+                print(jsonResp)
+                raise Exception('AAD Authentication error')
+
+            token = jsonResp['access_token']
+            subdomain = str(os.environ.get('SUBDOMAIN'))
+
+            return jsonify(token = token, subdomain = subdomain)
+        except Exception as e:
+            message = 'Unable to acquire Azure AD token. Check the debugger for more information.'
+            print(message, e)
+            return jsonify(error = message)
