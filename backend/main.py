aa7fcf114e3b769f0b57fe7360f61ed105684442
Dan <cornul11@gmail.com>
2021-03-20 18:14:42 +0100

    added file upload

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask_cors import CORS
+import sys
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, World!'
+    if request.method == 'GET':
+        return 'Hello, World!'
+    elif request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+        return redirect(url_for('hello_world'))
