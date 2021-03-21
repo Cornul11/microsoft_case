13885b9f13c5a46d905ddc11cb1864e12145e9c7
Dan <cornul11@gmail.com>
2021-03-21 04:01:36 +0100

    added dictionary definition search

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,11 +2,29 @@ import DragAndDrop from "./DragAndDrop";
 import React from "react";
 import FileList from "./FileList";
 import {ThemeProvider} from "styled-components";
+import {Input, Collapse} from "antd";
+import Searchbar from "./SearchBar";
 
+const {Search} = Input;
+const {Panel} = Collapse;
 
+function callback(key) {
+    console.log(key);
+}
 
-function Home() {
-    return <FileList />;
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Search for a word first.'
+        }
+    }
+    render() {
+        return (<div>
+            <Searchbar />
+            <FileList/>
+        </div>);
+    }
 }
 
 export default Home;
--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,64 +1,84 @@
 import React from 'react';
+import {Collapse} from "antd";
+import Search from "antd/es/input/Search";
 
-export default class Searchbar extends React.Component {
-    state = {
-        searchTerm: ""
+const {Panel} = Collapse;
+
+class Searchbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            definition: 'Look for a word first.',
+            loadingState: false,
+            searchField: ''
+        }
+
+        this.onPressEnter = this.onPressEnter.bind(this);
+        this.onSearch = this.onSearch.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
-    doingASearch = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+    onSearch = (value, e) => {
+        this.setState({loadingState: true});
+        this.submitRequest();
     }
 
-    filterBySearchTerm = (search) => {
-        this.setState({
-            theLocationFilter: search,
-        })
+    onPressEnter() {
+        this.setState({loadingState: true});
+        this.onSearch();
+    }
 
-        fetch("https://www.dictionaryapi.com/api/v3/references/medical/json/" + search + "?key=7635550d-59ba-4461-b911-7cda738f84a2", {
+    onChange = e => {
+        this.setState({searchField: e.target.value});
+    }
+
+
+    submitRequest() {
+        console.log(this.state.searchField);
+        fetch("https://www.dictionaryapi.com/api/v3/references/medical/json/" + this.state.searchField + "?key=7635550d-59ba-4461-b911-7cda738f84a2", {
             method: "GET",
-            // body: JSON.stringify({
-            //     query: search
-            // })
         })
             .then(r => r.json())
             .then(result => {
                 console.log(result[0].def[0].sseq[1][1][1].dt[0][1])
                 if (result[0].def[0].sseq[1][1][1].dt[0][1] > 0) {
                     this.setState({
-                        searchTerm: search,
-                        filterAll: false,
-                        searchPark: result.def[0].sseq[0][0].sense.sdsense.dt[0][0][1],
+                        definition: result.def[0].sseq[0][0].sense.sdsense.dt[0][0][1],
                         isLoading: false
                     })
                 } else {
                     this.setState({
-                        theLocationFilter: search,
-                        filterAll: false,
                         isLoading: false,
-                        searchError: "No definitions found"
+                        definition: "No definitions found"
                     })
                 }
             })
     }
 
-    submitSearch = (event) => {
-        event.preventDefault();
-        this.filterBySearchTerm(this.state.searchTerm)
-        this.setState({
-            searchTerm: ""
-        })
-    }
-
     render() {
         return (
-            <form onSubmit={this.submitSearch}>
-                <label htmlFor="searchTerm">
-                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.doingASearch}/>
-                    <input type="submit" value="Search"/>
-                </label>
-            </form>
+            // <form onSubmit={this.submitSearch}>
+            //     <label htmlFor="searchTerm">
+            //         <input type="text" name="searchTerm" value={this.state.searchTerm}
+            //                onChange={this.doingASearch}/>
+            //         <input type="submit" value="Search"/>
+            //     </label>
+            // </form>
+            <div>
+                <Search placeholder="Look for the definition of a word" enterButton
+                        size="medium"
+                        style={{width: '250px', paddingBottom: '5px'}}
+                        onSearch={this.onSearch}
+                        onChange={this.onChange}
+                        loading={this.state.loadingState}/>
+                <Collapse defaultActiveKey={[]}>
+                    <Panel header="Dictionary definition" key="1">
+                        <p>{this.state.definition}</p>
+                    </Panel>
+                </Collapse>
+            </div>
         )
     }
 }
+
+export default Searchbar;
